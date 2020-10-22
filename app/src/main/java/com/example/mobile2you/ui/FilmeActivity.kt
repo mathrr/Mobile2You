457543da976eab16db892bbceff36dfc03d26b67
Velocipanda <newmathrr@gmail.com>
2020-10-21 23:02:44 -0300

    Image from url using picasso No title app bar; Image Button non functional

--- app/src/main/java/com/example/mobile2you/ui/FilmeActivity.kt
+++ app/src/main/java/com/example/mobile2you/ui/FilmeActivity.kt
@@ -3,6 +3,8 @@ package com.example.mobile2you.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobile2you.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_filme_principal.*
 
 class FilmeActivity : AppCompatActivity() {
 
@@ -16,6 +18,8 @@ class FilmeActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_filme_principal)
+
+        Picasso.get().load("https://image.tmdb.org/t/p/w600_and_h900_bestv2/pci1ArYW7oJ2eyTo2NMYEKHHiCP.jpg").into(activity_filme_principal_image_view_principal)
 
     }
 
