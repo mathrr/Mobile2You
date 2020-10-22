428c9fbd54c8de80ed25d313c8002b228eb70707
Velocipanda <newmathrr@gmail.com>
2020-10-22 19:21:18 -0300

    Image from url using picasso; No title app bar; Image Button visually working onClick, changing it's image to a fill heart and vice-versa; Popularity placement on layout; Votecount placement on layout.

--- app/src/main/java/com/example/mobile2you/ui/FilmeActivity.kt
+++ app/src/main/java/com/example/mobile2you/ui/FilmeActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mobile2you.ui
 
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobile2you.R
@@ -9,31 +8,40 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_filme_principal.*
 
 
-class FilmeActivity : AppCompatActivity() {
+class FilmeActivity : AppCompatActivity(){
+
+    var URLBase = "https://api.themoviedb.org/"
+
+    var favoritado = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //Tirar a app title bar
-        try {
-            this.supportActionBar!!.hide()
-        } catch (e: NullPointerException) {
-        }
+        tiraAppTitleBar()
 
         setContentView(R.layout.activity_filme_principal)
 
         //pega imagem da internet
         Picasso.get()
-            .load("https://image.tmdb.org/t/p/w600_and_h900_bestv2/pci1ArYW7oJ2eyTo2NMYEKHHiCP.jpg")
+            .load("https://image.tmdb.org/t/p/w600_and_h900_bestv2/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg")
             .into(
-                activity_filme_principal_image_view_principal
+                activity_filme_principal_image_view_poster_filme_principal
             )
 
+        configuraBotaoFavoritar()
 
-        //botao de favorito
-        var favoritado = false
+    }
+
+    private fun tiraAppTitleBar() {
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+    }
+
+    private fun configuraBotaoFavoritar() {
         val botaoFavorito =
-            findViewById<ImageButton>(R.id.activity_filme_principal_image_button_favorite)
+            findViewById<ImageButton>(R.id.activity_filme_principal_image_button_favorito)
         botaoFavorito.setOnClickListener {
 
             botaoFavorito.isSelected = !botaoFavorito.isPressed()
@@ -41,14 +49,10 @@ class FilmeActivity : AppCompatActivity() {
             if (botaoFavorito.isPressed && !favoritado) {
                 botaoFavorito.setImageResource(R.drawable.ic_baseline_favorite_24)
                 favoritado = !favoritado
-            }
-
-            else if (botaoFavorito.isPressed && favoritado){
+            } else if (botaoFavorito.isPressed && favoritado) {
                 botaoFavorito.setImageResource(R.drawable.ic_baseline_favorite_border_24)
                 favoritado = !favoritado
-            }
-
-            else {
+            } else {
                 botaoFavorito.setImageResource(R.drawable.ic_baseline_favorite_border_24)
             }
         }
